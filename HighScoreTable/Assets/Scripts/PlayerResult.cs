f9481b7273d692b14adfdd5872d00e5e6816ab26
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-10-21 16:29:46 +0300

    player result

---
+++ HighScoreTable/Assets/Scripts/PlayerResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerResult : MonoBehaviour
+{
+    [SerializeField] Text playerName = null;
+    [SerializeField] Text playerLastName = null;
+    [SerializeField] Text playerPlace = null;
+    [SerializeField] Text playerPoints = null;
+
+    public void SetupResult(int place, string name, string lastName, int points)
+    {
+        playerPlace.text = (place+1).ToString();
+        playerName.text = name;
+        playerLastName.text = lastName;
+        playerPoints.text = points.ToString();
+    }
+}
