087c202f1e189ff88cf5ae099e6afca1ebe2df2b
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-10-21 16:31:03 +0300

    buttons

---
+++ HighScoreTable/Assets/Scripts/Buttons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    [SerializeField] Animator animator = null;
+
+    LeagueTable leagueTable;
+
+    private void Start()
+    {
+        leagueTable = animator.gameObject.GetComponent<LeagueTable>();
+    }
+
+    public void CloseButton()
+    {
+        animator.SetBool("isClosed", true);
+        animator.SetBool("isPushed", false);
+        leagueTable.SaveData();
+    }
+
+    public void PushButton()
+    {
+        animator.SetBool("isClosed", false);
+        animator.SetBool("isPushed", true);
+        leagueTable.LoadData();
+    }
+}
